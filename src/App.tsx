27cef1e2534de2994aed4b6920bacbd7c69d78c6
uniqfadilah <uniqfadilah@gmail.con>
2024-07-02 23:49:21 +0700

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router } from 'react-router-dom';
+import Layout from './lib/layout';
+import Routings from './lib/router/Routings';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Layout>
+          <Routings />
+        </Layout>
+      </Router>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
